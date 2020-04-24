7bef5ffc247526b1a3ebb7a7319029a82da55c07
ray.macneil <ray.macneil@raymonds-air.hitronhub.home>
2020-04-23 19:08:48 -0700

    sort overhaul completed - stil working some bugs

--- varmaker.m
+++ varmaker.m
@@ -1,50 +1,55 @@
-function VarNamesOut = varmaker(condLabel, triaLabel, loopTrial, numTrialRep, lab3, srtcor, lab4) %#ok<INUSD>
+% condLabel = ["C1", "C2", "C3"];
+% triaLabel = ["A", "B", "C", "D"];
+% loopTrial = 'true';
+% numTrialRep = 2;
+% lab3 = ["ST", "EN"];
+% srtcor = 'true';
+% lab4 = "IDX";
+
+function VarNamesOut = varmaker(condLabel, triaLabel, loopTrial, numTrialRep, lab3, srtcorL3, lab4) 
 % function VarNamesOut = VarMaker(condLabel, triaLabel, loopTrial,
 % numTrialRep, lab3, lab4)
 % condLabel <STR> A string array containing the condition/group labels
 % triaLabel <STR> A string array containing the trial labels
-% loopTrial <CHR> A character vector, 'true' OR 'false', to indicate if it
+% loopTrial <CHR> A character vector, 'true' OR 'false', to indicate if
 % there will be repeated trials/measures
 % numTrialRep <POS INTEGER> Number of trial type variations
 % lab3 <STR> A string array representing another component of the trials
 % lab4 <STR> A single string acting as an optional universal tag
 % Example: VarNamesOut = varmaker(["C1", "C2", "C3"],["A", "B", "C", "D"],... 
-%               'true', 3, ["ST", "EN"], "IDX") 
-
-% % % % % % % % % % % % % % % % % % % % % % % %
-% Ray R. MacNeil                              %
-% UBC, Department of Psychology, Vision Lab   %
-% Email: raymond.macneil@mail.utoronto.ca     %
-% % % % % % % % % % % % % % % % % % % % % % % %
+%               'true', 2, ["ST", "EN"], "IDX") 
 %
 % Useage Notes: This was made as a semi-dynamic function though there is
-% some specificity built into. To get the variable label order correct a
-% a different sorting pattern will likely be required. You can express the
-% sorting pattern as a vector of row indices. See below for more details.
+% some specificity built into it. To get the variable label order correct
+% in some case will necessitate the use of a different sorting pattern.
+% Currently there is a sorting mechanism in place that will preserve the order 
+% for the input values provided for lab3, and it should be straightforward to 
+% eventually expand this functionality for the other input arguments. 
+% 
+% Ray R. MacNeil                              
+% UBC, Department of Psychology, Vision Lab   
+% Email: raymond.macneil@mail.utoronto.ca     
 
 %% Exception Handling and Input Error
 
-if  nargin < 4 || ~exist('numTrialRep', 'var') || isempty(numTrialRep) ...
-        || ~isinteger(numTrialRep)
+if  nargin < 4 || ~exist('numTrialRep', 'var') || isempty(numTrialRep)...
+        || ~isnumeric(numTrialRep) || numTrialRep ~= round(numTrialRep)...
+        || ~isscalar(numTrialRep)
     numTrialRep = 0;
 end
-% 
+
 if  nargin < 5 || ~exist('lab3', 'var') || isempty(lab3) || ~isstring(lab3)
     lab3 = 1;
 end
-%  %
-if  nargin < 6 || ~exist('srtcor','var') || isempty(srtcor) || ~ischar(srtcor)
-    srtcor = 'false';    
+
+if  nargin < 6 || ~exist('srtcor','var') || isempty(srtcorL3) || ~ischar(srtcorL3)
+    srtcorL3 = 'false';    
 end
     
 if  nargin < 7 || ~exist('lab4', 'var') || isempty(lab4) || ~isstring(lab4)
     lab4 = NaN;
 end
-
-
     
- 
-
 %% Main Activity
 
 if ~isempty(loopTrial) && strcmp(loopTrial, 'true')
@@ -72,9 +77,11 @@ else
 end    
     
 if isstring(lab3)
-    VarNamesOut = repmat(VarNamesOut, numel(lab3), 1);
     
-    for ii = 1:numel(lab3)
+    L3numel = numel(lab3)
+    VarNamesOut = repmat(VarNamesOut, L3numel, 1);
+    
+    for ii = 1:L3numel
         idxSTrng = (rows2iter * ii) - (rows2iter - 1);
         idxENrng = rows2iter * ii;
         VarNamesOut(idxSTrng:idxENrng,:) = VarNamesOut(idxSTrng:idxENrng,:) +...
@@ -91,14 +98,15 @@ end
 % x >= 0 that represents the swapping pattern 
 VarNamesOut = sortrows(VarNamesOut);
 
-if strcmp(srtcor, 'true')
+if strcmp(srtcorL3, 'true')
     
-    
+
+    [M,~] = size(VarNamesOut);
     [~, srtIDXfwd] = sort(lab3);
     [~, srtIDXrev] = sort(srtIDXfwd);
-    srtVec = 0:1:numel(lab3)
+    srtVec = 0:1:L3numel-1
 
-    for ii = 1:numel(lab3):length(VarNamesOut)
+    for ii = 1:L3numel:M
         VarNamesOut(srtVec+ii,:) = VarNamesOut(srtIDXrev + (ii-1),:);
     end
 end
@@ -107,5 +115,3 @@ VarNamesOut = cellstr(reshape(VarNamesOut, 1, []));
 
 
 end
-
-
