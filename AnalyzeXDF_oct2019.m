62797ed8d07b925b0f14d618740bba3349d716a6
Raymond MacNeil <ray.macneil@Raymonds-Air.hitronhub.home>
2020-04-26 23:13:13 -0700

    branch EOG analysis pipeline

--- AnalyzeXDF_oct2019.m
+++ AnalyzeXDF_oct2019.m
@@ -1,16 +1,25 @@
-%   Analyze XDF file from Saccade Study
-%   by Jamie Dunkle, 
-%   August 2019, UBC Vision Lab
+% function [event, elink, opbci] = AnalyzeXDF_oct2019(plots)
 %
-%   Updated by Ray MacNeil (UBC, Vision Lab), October, 2019
-%   Analysis no longer fails if one of the LSL streams is absent
-%   Visualization of data improved, segregated plots for X and Y gaze data 
-
-
+% INPUT:
+% plots <CHR> A character vector set to either 'true' or 'false' indicates
+% whether or not to include the lots in the output.
+%
+% OUTPUT (all arguments required):
+% event <STRUCT> gives the event struct from the XDF file
+% elink <STRUCT> gives the EyeLink data struct from the XDF file
+% opbci <STRUCT> gives the OpenBCI (EOG) data struct from the XDF file
+% 
+% Analyze XDF file from Saccade Study
+% by Jamie Dunkle, August 2019, UBC Vision Lab
+%
+% Updated by Ray MacNeil (UBC, Vision Lab), October, 2019
+% Analysis no longer fails if one of the LSL streams is absent
+% If requested, subplots for X and Y gaze data is given in the output
+function [event, elink, opbci] = AnalyzeXDF_oct2019(plots)
 SRATE = 250; % sammpling rate
 ISI = 1/SRATE; % inter-sample-interval
 
-[xdf_fname, xdf_path] = uigetfile('..\Data\*.xdf', 'Select a File');
+[xdf_fname, xdf_path] = uigetfile('*.xdf', 'Select a File');
 xdf = load_xdf([xdf_path, xdf_fname]);
 
 % Initialize variables
@@ -62,7 +71,7 @@ elseif ~elink_strm_exist
    elink = NaN; 
    el_time_axis = NaN; 
    el_dat_x = NaN; 
-   el_dat_y = NaN;
+   el_dat_y = NaN; 
    el_dat_x(el_dat_x < 0) = NaN; 
    el_dat_x(el_dat_x > 1920) = NaN;
    el_dat_y(el_dat_y < 0) = NaN; 
@@ -81,8 +90,10 @@ if opbci_strm_exist
 
 elseif ~opbci_strm_exist
 
-    opbci = NaN;     ob_time_axis = NaN;
-    ob_dat_x = NaN;  ob_dat_y = NaN;
+    opbci = NaN;     
+    ob_time_axis = NaN;
+    ob_dat_x = NaN;  
+    ob_dat_y = NaN;
 
 end
 
@@ -91,11 +102,11 @@ end
 % ob_dat = ob_dat - mean(ob_dat);
  ob_dat_x = detrend(ob_dat_x);
  ob_dat_y = detrend(ob_dat_y);
- ob_dat_x = bandpass(ob_dat_x,[0.6,35],250);
- ob_dat_y = bandpass(ob_dat_y,[0.6,35],250);
+ ob_dat_x = bandpass(ob_dat_x,[0.6, 35], 250);
+ ob_dat_y = bandpass(ob_dat_y,[0.6, 35], 250);
  
- %ob_dat_x = sgolayfilt(double(ob_dat_x),5,111);
- %ob_dat_y = sgolayfilt(double(ob_dat_y),5,111);
+% ob_dat_x = sgolayfilt(double(ob_dat_x),5,111);
+% ob_dat_y = sgolayfilt(double(ob_dat_y),5,111);
 % 
 % Wo = 60/(250/2);  
 % BW = Wo/35;
@@ -141,92 +152,62 @@ el_time_axis = el_time_axis - t1;
 ob_time_axis = ob_time_axis - t1;
 ev_ts = ev_ts - t1;
 
-fig=figure;
+if strcmp(plots, 'true')
 
-%%% Create Subplot for Gaze X Data %%%
-subplot(2,1,1); 
-hold on;
-plot(el_time_axis, el_dat_x, 'Color', [0, 0.447, 0.741]);
-plot(ob_time_axis, detrend(ob_dat_x),'Color', [0.635 0.078 0.184]);
-title('Horizontal Gaze');
-xlabel('Time (secs)');
-ylabel('Gaze Position (pixels)');
-hold off
-legend({'EyeLink X', 'EOG X'}, 'AutoUpdate', 'off');
-
-%%% Create Subplot for Gaze Y Data %%%
-subplot(2,1,2);
-hold on;
-plot(el_time_axis, el_dat_y, 'Color', [0.929, 0.694, 0.125]);
-plot(ob_time_axis, detrend(ob_dat_y), 'Color', [0.494, 0.184, 0.556]);
-title('Vertical Gaze')
-xlabel('Time (secs)')
-ylabel('Gaze Position (pixels)')
-hold off
-legend({'EyeLink Y', 'EOG Y'}, 'AutoUpdate', 'off');
-
-for jj = 1:2
-    subplot(2,1,jj)
-    h = gca;
+    fig=figure;
     
-for ii=1:length(ev_ts)
-    event_tag = event.time_series{ii};
-    event_tag_split = strsplit(event_tag,'_');
-    if startsWith(event_tag_split{1}, 't')
-        if ~strcmp(event_tag_split{2}, 'cal') && strcmp(event_tag_split{2}, 'exp')
-            if strcmp(event_tag_split{4}, 'start')
-                line([ev_ts(ii) ev_ts(ii)], h.YLim,'Color',[0.1 0.8 0.1])
-            elseif strcmp(event_tag_split{4}, 'ping')
-                line([ev_ts(ii) ev_ts(ii)], h.YLim,'Color',[0 1 1])
-            elseif strcmp(event_tag_split{4}, 'end')
-                line([ev_ts(ii) ev_ts(ii)], h.YLim,'Color',[1 0 0])
-            end
-        elseif strcmp(event_tag_split{2}, 'calib') % && strcmp(event_tag_split{2}, 'exp')
-            if strcmp(event_tag_split{length(event_tag_split)}, 'start')
-                line([ev_ts(ii) ev_ts(ii)], h.YLim,'Color',[0.1 0.8 0.1])
-            elseif strcmp(event_tag_split{length(event_tag_split)}, 'ping')
-                line([ev_ts(ii) ev_ts(ii)], h.YLim,'Color',[0 1 1])
-            elseif strcmp(event_tag_split{length(event_tag_split)}, 'end')
-                line([ev_ts(ii) ev_ts(ii)], h.YLim, 'Color',[1 0 0])
-            end
-        end
+    %%% Create Subplot for Gaze X Data %%%
+    subplot(2,1,1);
+    hold on;
+    plot(el_time_axis, el_dat_x, 'Color', [0, 0.447, 0.741]);
+    plot(ob_time_axis, detrend(ob_dat_x),'Color', [0.635 0.078 0.184]);
+    title('Horizontal Gaze');
+    xlabel('Time (secs)');
+    ylabel('Gaze Position (pixels)');
+    hold off
+    legend({'EyeLink X', 'EOG X'}, 'AutoUpdate', 'off');
+
+    %%% Create Subplot for Gaze Y Data %%%
+    subplot(2,1,2);
+    hold on;
+    plot(el_time_axis, el_dat_y, 'Color', [0.929, 0.694, 0.125]);
+    plot(ob_time_axis, detrend(ob_dat_y), 'Color', [0.494, 0.184, 0.556]);
+    title('Vertical Gaze')
+    xlabel('Time (secs)')
+    ylabel('Gaze Position (pixels)')
+    hold off
+    legend({'EyeLink Y', 'EOG Y'}, 'AutoUpdate', 'off');
+    
+    
+    
+    for jj = 1:2
+        subplot(2,1,jj)
+        h = gca;
+        
+        for ii=1:length(ev_ts)
+            event_tag = event.time_series{ii};
+            event_tag_split = strsplit(event_tag,'_');
+            if startsWith(event_tag_split{1}, 't')
+                if ~strcmp(event_tag_split{2}, 'cal') && strcmp(event_tag_split{2}, 'exp')
+                    if strcmp(event_tag_split{4}, 'start')
+                        line([ev_ts(ii) ev_ts(ii)], h.YLim,'Color',[0.1 0.8 0.1])
+                    elseif strcmp(event_tag_split{4}, 'ping')
+                        line([ev_ts(ii) ev_ts(ii)], h.YLim,'Color',[0 1 1])
+                    elseif strcmp(event_tag_split{4}, 'end')
+                        line([ev_ts(ii) ev_ts(ii)], h.YLim,'Color',[1 0 0])
+                    end
+                elseif strcmp(event_tag_split{2}, 'calib') % && strcmp(event_tag_split{2}, 'exp')
+                    if strcmp(event_tag_split{length(event_tag_split)}, 'start')
+                        line([ev_ts(ii) ev_ts(ii)], h.YLim,'Color',[0.1 0.8 0.1])
+                    elseif strcmp(event_tag_split{length(event_tag_split)}, 'ping')
+                        line([ev_ts(ii) ev_ts(ii)], h.YLim,'Color',[0 1 1])
+                    elseif strcmp(event_tag_split{length(event_tag_split)}, 'end')
+                        line([ev_ts(ii) ev_ts(ii)], h.YLim, 'Color',[1 0 0])
+                    end
+                end
+            end      
+        end     
     end
 end
+
 end
-
-
-% subplot(2,1,2);
-% % hax2=axes;
-% hold on
-% % plot(el_time_axis,el_dat_x);
-% % title('Eye Movement HREF - Raw Data')
-% xlabel('Time (secs)')
-% % ylabel('HREF Value')
-% plot(ob_time_axis, ob_dat_filt)
-% h=findobj(gcf,'type','axes');
-% for ii=1:length(ev_ts)
-%     event_tag = event.time_series{ii};
-%     event_tag_split = strsplit(event_tag,'_');
-%     if ~strcmp(event_tag_split{1}, 'cal') && ~strcmp(event_tag_split{1}, 'exp')
-%         if strcmp(event_tag_split{3}, 'start')
-%             line([ev_ts(ii) ev_ts(ii)],get(h(1),'YLim'),'Color',[0.1 0.8 0.1])
-%         elseif strcmp(event_tag_split{3}, 'end')
-%             line([ev_ts(ii) ev_ts(ii)],get(h(1),'YLim'),'Color',[1 0 0])
-%         end 
-%     end
-% end
-
-% T = readtable('C:\Users\Visionlab\Documents\Processing\OpenBCI_GUI\OpenBCI_GUI\SavedData\OpenBCI-RAW-2019-03-06_14-18-11.txt',...
-%         'Delimiter',',','ReadVariableNames',false);
-% EOG_dat = T.Var3;
-% EOG_ts = T.Var13;
-% EOG_ts = datenum(EOG_ts);
-% 
-% [~,idx] = min(abs(EOG_dat-ob_dat_raw(end)));
-% bci_dat_raw = EOG_dat(idx-length(ob_dat_raw)+1:idx)';
-% figure;hold on;plot(eog_raw_dat);plot(ob_dat_raw)
-% 
-% EEGDa_ts = [];
-% for i = 1:length(VarName13)
-%     EEGDa_ts = strsplit(VarName13(1),',');
-% end
---
+++ linear_calibration.m
@@ -0,0 +1,146 @@
+function [calibration_factor] = linear_calibration(event_struct, data, threshold)
+
+event_struct = event_struct;  % struct containing time_series (labels) and time_stamps (times)
+data_struct = data; % Same as above, for EOG/Elink data
+
+calibration_angles = [-22 -11 11 22];
+
+% Struct with letter key, matrix value corresponding to each letter 
+% [start ping end] for each row
+times = struct('A', [], 'B', [], 'C', [], 'D', []);
+points = fieldnames(times);
+ 
+% Get start and stop times
+% Extract [start ping end] times from time series and time stamps
+% Assume ping and stop always follow start, in that order
+for ind = 1:length(event_struct.time_series)
+    if strcmp(event_struct.time_series(ind), "exp_start")
+        exp_start_ind = ind;
+        break
+    end
+    
+    % Extract tag name, eg, t1_calib_H_A_start
+    tag = strsplit(event_struct.time_series{ind},'_');  
+    
+    % If calibration and horizontal
+    if startsWith(tag{1}, 't') ...
+      && strcmp(tag{2}, 'calib') ...
+      && strcmp(tag{3}, 'H') ...
+      && strcmp(tag{5}, 'start')
+        % Validate
+        if ~ismember(tag{4}, points)
+            error(tag{4} + " not a valid point")
+        else
+            % Add to matrix
+            toAdd = [event_struct.time_stamps(ind) event_struct.time_stamps(ind+1) event_struct.time_stamps(ind+2)];
+            times.(tag{4}) = [times.(tag{4}); toAdd];
+        end
+    end
+end
+
+% Values of each saccade
+values = struct('A', [], 'B', [], 'C', [], 'D', []);
+
+% For each point, add corresponding saccade voltage value to array - taking
+% points between (start + (ping-start)/2) and ping
+dat_x = data_struct.time_series;
+% opbci_dat_x = data_struct.time_series(1, :);
+% opbci_dat_x = detrend(opbci_dat_x);
+% opbci_dat_x = bandpass(opbci_dat_x,[0.6,35],250);
+
+for i = 1:length(points)
+    point = points{i};
+    
+    % Loop through each row of times [start ping stop]
+    for row = 1:size(times.(point), 1)
+        start = times.(point)(row, 1);
+        ping = times.(point)(row, 2);
+        stop = times.(point)(row, 3);
+        saccade = []; % Single saccade
+      
+        
+        % Loop through data
+        for ind = 1:length(data_struct.time_stamps)
+            
+            if data_struct.time_stamps(ind) > stop
+                break
+            end
+            
+            if data_struct.time_stamps(ind) >= start
+               saccade = [saccade dat_x(ind)]; % Append to array
+            end
+            
+        end
+        
+        cutoffs = findchangepts(saccade, "MaxNumChanges", 2); % Isolate saccade using changepoints
+        
+        % If insufficient changepoints found
+        if length(cutoffs) < 2
+%               continue
+              cutoffs = [1 length(saccade)];
+        end
+        
+        %Plot individual saccades
+%         figure()
+%         plot(saccade)
+%         xline(cutoffs(1));
+%         xline(cutoffs(2));
+
+        % Only take peaks of saccade
+        peaks = findpeaks(abs(saccade(cutoffs(1): cutoffs(2))));
+        % If there are no peaks in isolated region, then take entire length
+        if(length(peaks) == 0)
+            peaks = findpeaks(abs(saccade));
+        end
+        peaks = peaks(peaks > threshold(1) & peaks < threshold(2));
+        
+        if strcmp(point, 'A') || strcmp(point, 'B')
+            peaks = -peaks;
+        end
+        values.(point) = [values.(point) peaks]; % Append to all values
+    end
+end
+
+% Remove outliers - 1.5*IQR from mean
+for i = 1:length(points)
+    point = points{i};
+    
+    val_avg = mean(values.(point));
+    val_iqr = iqr(values.(point));
+    
+    val_filter = abs(values.(point)-val_avg) < 1.5*val_iqr;
+    
+    % Increase IQR until more than 80% of data points remain
+    iqr_scale = 1.5;
+    while nnz(val_filter)/numel(val_filter) < 0.8
+        iqr_scale = iqr_scale + 0.1;
+        val_filter = abs(values.(point)-val_avg) < iqr_scale*val_iqr;
+    end
+    
+    values.(point) = values.(point)(val_filter);
+end
+
+% Calculate mean and error of each point
+average_voltages = zeros(1, length(points));
+errors = zeros(1, length(points));
+
+for i = 1:length(points)
+    average_voltages(i) = mean(values.(points{i}));
+    errors(i) = std(values.(points{i}));
+end
+
+% Linear fit - m is calibration factor: angle = m*voltage
+calibration_factor = calibration_angles/average_voltages;
+
+% Plots
+figure()
+errorbar(calibration_angles,average_voltages, errors, ...
+   's','MarkerSize',5,'MarkerEdgeColor','black','MarkerFaceColor','black')
+title('Calibration Curve : Voltage vs. Angle Plot');
+hold on 
+plot(calibration_angles, calibration_angles/calibration_factor);
+xlabel('Saccade size - degrees'); 
+ylabel('Voltage -  milli volts'); 
+legend('Calibration experimental data','Fitted curve');
+
+end
